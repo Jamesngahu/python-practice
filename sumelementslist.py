b99f0cc1812022fc70bebde94f46b243073f54b1
jamom <jamesmathengengahu@gmail.com>
2025-04-26 10:03:57 +0300

    update

--- sumelementslist.py
+++ sumelementslist.py
@@ -1,8 +1,14 @@
-def sum(score):
-    sum = 0
+#created a function total
+def total(score):
+    #initialised the local variable value to zero
+    total = 0
     for n in score:
-        sum = sum + n
-    return sum
+        total = total + n
+        #created a loop that stores the value as it goes through items in the list in n
+    return total
 my_scores=[51, 11, 10, 5]
-result = sum(my_scores)
+#created a scores list
+result = total(my_scores)
+#created a variable  result that calls the total function
 print(f"The total score is: {result}")
+#included an f-string to call the variable result
